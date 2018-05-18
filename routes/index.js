39e4f60469bfa2dfa4ada6fb4337fe09192cb91c
Michael <korzunm@gmail.com>
2018-05-18 12:09:39 +0300

    ready to start install apollo server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const os = require('os');
+const data = require('../utils');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(data)
+  res.render("index", { title: "Express", data });
 });
 
 module.exports = router;
